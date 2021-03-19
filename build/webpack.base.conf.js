023c2a996be4bbb2cb762f94a13ef3e28abc2dfe
Denis Hardenberg <6463690+vkdg@users.noreply.github.com>
2021-03-19 17:16:24 +0500

    update structure, fix bugs

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,7 +3,6 @@ const path = require('path')
 const fs = require('fs')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const PATHS = {
   src: path.join(__dirname, '../src'),
