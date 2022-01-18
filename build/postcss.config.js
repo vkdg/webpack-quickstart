0f46642507f1c408cfec730fe09438a6c3ce6ee6
Denis Hardenberg <6463690+vkdg@users.noreply.github.com>
2022-01-18 11:39:09 +0500

    Update all devDependencies

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -3,15 +3,6 @@
 
 module.exports = {
   	plugins: [
-    	require('autoprefixer'),
-    	require('cssnano')({
-      		preset: [
-        		'default', {
-          			discardComments: {
-            			removeAll: true
-          			}
-        		}
-      		]
-    	})
+    	require('autoprefixer')
   	]
 }
